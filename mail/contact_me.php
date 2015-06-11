c1b4b720ce0be816f31df88be19ba19f70908ab3
Zeshan Sajid <zsajid@hotmail.com>
2015-06-11 21:17:10 +0100

    Try
    Try

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,7 +20,6 @@ $to = 'thezeshansajid@gmail.com'; // Add your email address inbetween the '' rep
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
